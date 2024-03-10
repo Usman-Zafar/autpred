780ebdeca137e99905d2d4f16c71b1e105d53141
Mohammad Usman Zafar <usmanzafar622@gmail.com>
2024-03-11 04:11:39 +0500

    Care Giver Dashboard Complete

--- src/app/caregiverdashboard/page.tsx
+++ src/app/caregiverdashboard/page.tsx
@@ -1,25 +1,67 @@
 import React from "react";
 import CaregiverNavbar from "@/components/CaregiverTherapist";
 import Link from "next/link";
+import Image from "next/image";
+import Graph from "../../../public/Graph.png";
 
 const caregiverdashboard = () => {
   return (
     <>
       <CaregiverNavbar />
-      <div className="center-content flex flex-col flex-1 justify-center items-center">
-        <h1 className="text-3xl font-bold text-center mb-4">Welcome User</h1>
-
-        <div className="button-group flex flex-row justify-center gap-2">
-          <Link href="/asd-screening">
-            <button className="px-4 py-2 rounded bg-transparent border-green-500 border text-black hover:bg-green-600">
+      <div className="center-content flex flex-col items-center flex-1 mt-4 w-full">
+        <div className="flex justify-center items-center gap-x-2 w-full px-2">
+          <Link href="/asd-screening" className="w-1/2">
+            <button className="px-4 py-2 rounded bg-transparent w-full border-green-500 border text-black hover:bg-green-600">
               ASD Screening
             </button>
           </Link>
-          <Link href="/view-reports">
-            <button className="px-4 py-2 rounded bg-transparent border-green-500 border text-black hover:bg-green-600">
+          <Link href="/view-reports" className="w-1/2">
+            <button className="px-4 py-2 rounded bg-transparent w-full border-green-500 border text-black hover:bg-green-600">
               Monthly Report
             </button>
           </Link>
+        </div>
+        <div className="flex gap-x-2 w-full px-2">
+          <div className="w-1/2 px-2">
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child look at you when you call his/her name?
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              How easy is it for you to get eye contact with your child?{" "}
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child point to indicate that s/he wants something?
+              (e.g., a toy that is out of reach){" "}
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child point to share interest with you? (e.g., pointing
+              at an interesing sight){" "}
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child pretend? (e.g., care for dolls, talk on a toy
+              phone){" "}
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child follow where you’re looking?{" "}
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              If you or someone else in the family is visibly upset, does your
+              child show signs. of wanting to comfort them? (e.g., stroking
+              hair, hugging them)
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Would you describe your child’s first words as:
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child use simple gestures? (e.g., wave goodbye)
+            </p>
+            <p className=" bg-white flex flex-col w-full text-left rounded-lg shadow-md p-2 my-2">
+              Does your child stare at nothing with no apparent purpose?
+            </p>
+          </div>
+          <div className="w-1/2 px-2">
+            <Image src={Graph} alt="Graph" />
+          </div>
         </div>
       </div>
     </>
