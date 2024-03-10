5124fa472d033a4c7e1c82f8fcd01570e42b6a3c
Mohammad Usman Zafar <usmanzafar622@gmail.com>
2024-03-10 23:45:33 +0500

    FAQ Page Added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeNavigation from "../../components/HomeNavigation";
+
+const AboutUs = () => {
+  return (
+    <>
+      <HomeNavigation />
+      <div className=""></div>
+    </>
+  );
+};
+
+export default AboutUs;
